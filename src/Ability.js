873bb46dfe7ebdfd63212d77b22b5b81be186ab5
Zachary Sarver <Zachary.Sarver@gmail.com>
2017-05-14 16:57:00 -0500

    Abilities now store time
    Need to work on: ability cooldown, ability create form

--- src/Ability.js
+++ src/Ability.js
@@ -3,20 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class Ability extends Component {
-  constructor(props) {
-      super(props);
-      this.state={active: false};
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false,
+            lastActivated: null,
+        };
 
-      this.activate = this.activate.bind(this);
-  }  
-  render() {
-    return (
-        <div onClick={this.activate}><p>Nobody here but us chickens!</p></div>
-    );
-  }
-  activate() {
-      this.setState({active: true});
-  }
+        this.activate = this.activate.bind(this);
+    }
+    render() {
+        var body;
+        if (this.state.active) {
+            body = <div><p>Activated at {this.state.lastActivated.toString()}</p></div>;
+        }
+        else {
+            body = <div onClick={this.activate}><p>Nobody here but us chickens!</p></div>;
+        }
+        return body;
+    }
+    activate() {
+        var d = new Date();
+        this.setState({ active: true, lastActivated: d });
+    }
 }
 
 export default Ability;
--- src/Ability.test.js
+++ src/Ability.test.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import Ability from './Ability';
 
+const ability = shallow(<Ability />);
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Ability />, div);
 });
 
 it('can be clicked to be activated', () => {
-    const ability = shallow(<Ability />);
-    expect(ability.state().active).toBeFalsy();
-    ability.simulate('click');
-    expect(ability.state().active).toBeTruthy();
+  expect(ability.state().active).toBeFalsy();
+  ability.simulate('click');
+  expect(ability.state().active).toBeTruthy();
+})
+
+it('saves the time it was clicked when activated', () => {
+  ability.simulate('click');
+  var d = new Date();
+  var diff = Math.abs(ability.state().lastActivated.getTime() - d.getTime());
+  expect(diff).toBeLessThan(100);
 })
