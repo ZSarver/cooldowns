169e486e5cdf6f86ae8aa70c79b28bd70cfcf58f
Zachary Sarver <Zachary.Sarver@gmail.com>
2017-05-15 16:57:30 -0500

    AbilityBox basic tests written

---
+++ src/AbilityBox.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class AbilityBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { abilities: {} };
+    }
+
+    render() {
+        return(<p>Nobody here but us chickens!</p>);
+    }
+    
+}
+
+export default AbilityBox;
